ef28dc8d7a8dc70c05f6acb009f9d919cec445b8
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2019-06-12 23:23:55 -0500

    whatever

--- mastermold/version.py
+++ mastermold/version.py
@@ -3,6 +3,6 @@ import pkg_resources
 
 try:
     __version__ = pkg_resources.require("mastermold")[0].version
-except Exception:
+except Exception:  # pragma:no cover
     # Run pytest without needing to install the library
     __version__ = None
